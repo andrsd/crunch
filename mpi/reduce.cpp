8eeb401ba44f064991a48022872dc8135366ebaa
David Andrs <andrsd@gmail.com>
2026-01-02 14:30:06 -0700

    Adding test for MPI reduce for double

--- mpi/reduce.cpp
+++ mpi/reduce.cpp
@@ -71,6 +71,63 @@ reduce_int_min(benchmark::State & state)
     }
 }
 
+static void
+reduce_double_sum(benchmark::State & state)
+{
+    mpi::Communicator comm;
+
+    for (auto _ : state) {
+        comm.barrier();
+        double num = (double) std::rand() / RAND_MAX;
+        double red;
+        auto t0 = mpi::wall_time();
+        comm.reduce(num, red, mpi::op::sum<double>(), 0);
+        auto t1 = mpi::wall_time();
+
+        double max_elapsed_secs;
+        comm.all_reduce(t1 - t0, max_elapsed_secs, mpi::op::max<double>());
+        state.SetIterationTime(max_elapsed_secs);
+    }
+}
+
+static void
+reduce_double_max(benchmark::State & state)
+{
+    mpi::Communicator comm;
+
+    for (auto _ : state) {
+        comm.barrier();
+        double num = std::rand();
+        double red;
+        auto t0 = mpi::wall_time();
+        comm.reduce(num, red, mpi::op::max<double>(), 0);
+        auto t1 = mpi::wall_time();
+
+        double max_elapsed_secs;
+        comm.all_reduce(t1 - t0, max_elapsed_secs, mpi::op::max<double>());
+        state.SetIterationTime(max_elapsed_secs);
+    }
+}
+
+static void
+reduce_double_min(benchmark::State & state)
+{
+    mpi::Communicator comm;
+
+    for (auto _ : state) {
+        comm.barrier();
+        double num = std::rand();
+        double red;
+        auto t0 = mpi::wall_time();
+        comm.reduce(num, red, mpi::op::min<double>(), 0);
+        auto t1 = mpi::wall_time();
+
+        double max_elapsed_secs;
+        comm.all_reduce(t1 - t0, max_elapsed_secs, mpi::op::max<double>());
+        state.SetIterationTime(max_elapsed_secs);
+    }
+}
+
 // clang-format off
 BENCHMARK(reduce_int_sum)
     ->Setup(do_setup)
@@ -83,6 +140,22 @@ BENCHMARK(reduce_int_max)
     ->UseManualTime()
 ;
 BENCHMARK(reduce_int_min)
+    ->Setup(do_setup)
+    ->Teardown(do_teardown)
+    ->UseManualTime()
+;
+
+BENCHMARK(reduce_double_sum)
+    ->Setup(do_setup)
+    ->Teardown(do_teardown)
+    ->UseManualTime()
+;
+BENCHMARK(reduce_double_max)
+    ->Setup(do_setup)
+    ->Teardown(do_teardown)
+    ->UseManualTime()
+;
+BENCHMARK(reduce_double_min)
     ->Setup(do_setup)
     ->Teardown(do_teardown)
     ->UseManualTime()
